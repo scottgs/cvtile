7921670f3b717f5145b6e1161b8382c463b6d0f9
Grant Scott <GrantScott@missouri.edu>
2015-09-14 11:49:51 -0500

    fixing parameter order for ROI construction

--- tests/TilerTestSuite.hpp
+++ tests/TilerTestSuite.hpp
@@ -297,21 +297,23 @@ class TilerTestSuite : public CxxTest::TestSuite
 			TS_ASSERT(NoError == read_tiler.open(sourceFile));
 
 			//tile Size
-			const cv::Size2i sz(16,16);
+			const cv::Size2i sz(256,256);
 
 			//set the cvTileSize so the image will be partitioned based on this
 			read_tiler.setCvTileSize(sz);
 
-			//loop through all tiles and put them into the writer Tiler
-			for (int i = 0; i < read_tiler.getCvTileCount(); ++i)
-			{
-					// READ WITH BUFFER
-				cvt::cvTile<unsigned char> iTile = read_tiler.getCvTile<unsigned char>(i, 5);
-				TS_ASSERT_EQUALS(iTile.getROI().x,5);
-				TS_ASSERT_EQUALS(iTile.getROI().y,5);
-				TS_ASSERT_EQUALS(iTile.getROI().size().width,16);
-				TS_ASSERT_EQUALS(iTile.getROI().size().height,16);
-			}
+				// READ WITH BUFFER
+				// Tile 4, buffer 10
+			cvt::cvTile<unsigned char> iTile = read_tiler.getCvTile<unsigned char>(4, 10);
+			
+			TS_ASSERT_EQUALS(iTile.getSize().width, 276);
+			TS_ASSERT_EQUALS(iTile.getSize().width, 276);
+
+			TS_ASSERT_EQUALS(iTile.getROI().x,10);
+			TS_ASSERT_EQUALS(iTile.getROI().y,10);
+			
+			TS_ASSERT_EQUALS(iTile.getROI().size().width,256);
+			TS_ASSERT_EQUALS(iTile.getROI().size().height,256);
 
 			read_tiler.close();
 		}
