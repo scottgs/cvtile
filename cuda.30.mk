926096b4ece19daf3b41da88a53b36c43f8adae3
Andrew Craighead <apcvzb@mail.missouri.edu>
2016-08-30 14:18:02 -0500

    Minimum supported compute = 3.5. No symlinking required for install.

--- cuda.30.mk
+++
@@ -1,6 +0,0 @@
-.cu.o:
-	$(NVCC) -gencode=arch=compute_30,code=sm_30 -o $@ -c $<
-
-.cu.lo:
-	$(top_srcdir)/cudalt.py $@ $(NVCC) -gencode=arch=compute_30,code=sm_30 --compiler-options=\" $(CFLAGS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) \" -c $<
-
--- src/Cuda4or5.h
+++ src/Cuda4or5.h
@@ -1,26 +1,22 @@
-// One header to rule them all
 // This header will include the CUDART and determine the
-//		appropriate CUDA SDK headers to include
-//CUDA includes
-//#include "/opt/cuda/include/cuda_runtime_api.h"
+// appropriate CUDA SDK headers to include
+// CUDA includes
+
 #include <cuda_runtime_api.h>
 
 // The CUDART API sets MACRO CUDART_VERSION
-#if CUDART_VERSION >= 8000
-#pragma message("Using CUDA Toolkit 8")
-#include <cuda.h>
 
-#elif CUDART_VERSION >= 7000
-#pragma message("Using CUDA Toolkit 7")
+#if CUDART_VERSION >= 7000
+//#pragma message("Using CUDA Toolkit 7 or higher")
 //#include <help_cuda.h>
 #include <cuda.h>
 
 #elif CUDART_VERSION >= 5000
-#pragma message("Using CUDA Toolkit 5")
+//#pragma message("Using CUDA Toolkit 5")
 #include <helper_functions.h>
 
 #elif CUDART_VERSION >= 4000
-#pragma message("Using CUDA Toolkit 4")
+//#pragma message("Using CUDA Toolkit 4")
 #include <sdkHelper.h>
 #include <shrQATest.h>
 #include <shrUtils.h>
